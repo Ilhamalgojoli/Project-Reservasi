ac3bb4eb297a278811c07bc317afa06c00e7fca8
ilhom <rilhamgojali123@gmail.com>
2025-12-04 14:01:51 +0700

    Update ubah padding box menjadi panjang lebar

--- resources/views/dashboard/kelola-gedung.blade.php
+++ resources/views/dashboard/kelola-gedung.blade.php
@@ -12,7 +12,7 @@
         <h1 class="font-bold text-2xl">{{ $title }}</h1>
         <button id="btn-gedung"
             class="
-            bg-[#FF0101] rounded-lg px-5 py-2 font-extrabold transition-all 
+            bg-[#FF0101] rounded-lg px-5 py-2 font-extrabold transition-all
             duration-300 hover:scale-105 flex justify-center items-center gap-2">
             <iconify-icon icon="mingcute:plus-fill" class="text-2xl"></iconify-icon>
             <span class="font-extrabold mt-1">Tambah Gedung</span>
@@ -59,12 +59,12 @@
                     </div>
                 </div>
                 <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] px-3.5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Ruangan</p>
                     </div>
-                    <div class="bg-[#FF0101] px-5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Lantai</p>
@@ -110,12 +110,12 @@
                     </div>
                 </div>
                 <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] px-3.5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Ruangan</p>
                     </div>
-                    <div class="bg-[#FF0101] px-5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Lantai</p>
@@ -161,12 +161,12 @@
                     </div>
                 </div>
                 <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] px-3.5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Ruangan</p>
                     </div>
-                    <div class="bg-[#FF0101] px-5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Lantai</p>
@@ -212,12 +212,12 @@
                     </div>
                 </div>
                 <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] px-3.5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Ruangan</p>
                     </div>
-                    <div class="bg-[#FF0101] px-5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Lantai</p>
@@ -263,63 +263,12 @@
                     </div>
                 </div>
                 <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] px-3.5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Ruangan</p>
                     </div>
-                    <div class="bg-[#FF0101] px-5 py-2 rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Lantai</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Card One -->
-        <div class="bg-[#ffffff] text-black rounded-xl py-1 px-1
-                shadow-lg space-x-4 flex flex-col">
-            <div class="flex flex-col justify-center items-center gap-5 mb-2">
-                <div class="flex flex-col gap-2">
-                    <div class="relative w-auto h-64 card-container">
-
-                        <img src="{{ asset('assets/basila_images/gambar-gedung.png') }}"
-                            class="w-full h-full object-cover rounded-lg" />
-                        <div class="overlay absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-lg z-20 hidden"
-                            ></div>
-
-                        <div class="button-kelola absolute top-[110px] left-[150px] z-30 hidden">
-                            <a id="btn-gedung" href="{{ route("index7") }}"
-                                class="bg-[#FF0101] rounded-lg px-5 py-2 font-extrabold transition-all cursor-pointer
-                                duration-300 hover:scale-105 flex justify-center items-center gap-2 mx-auto text-white">
-                                <iconify-icon icon="mingcute:plus-fill" class="text-2xl"></iconify-icon>
-                                <span class="font-extrabold mt-1">Tambah Gedung</span>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="flex flex-col px-5 gap-3">
-                        <div class="flex flex-row gap-10 justify-between items-center">
-                            <h2 class="text-black font-bold leading-tight md:text-xl sm:text-md lg:text-2xl">Nama
-                                Gedung
-                            </h2>
-                            <iconify-icon icon="basil:edit-outline" class="text-4xl opacity-50"
-                                id="edit-gedung"></iconify-icon>
-                        </div>
-                        <p class=
-                               "text-[#8B8B8B] text-md">
-                            надвигается туман, надвигается туман, помоги мне, позволь ему помочь тебе, кожа
-                            на моем теле ослабевает, я хочу ее снять, я чувствую, как растут новые конечности, это
-                            больно,
-                            ...</p>
-                    </div>
-                </div>
-                <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] px-3.5 py-2 rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Ruangan</p>
-                    </div>
-                    <div class="bg-[#FF0101] px-5 py-2 rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Lantai</p>
@@ -352,7 +301,7 @@
                 <div class="flex sm:flex-col md:flex-col lg:flex-row gap-5 mb-5">
                     <div class="flex flex-col gap-5 flex-1">
                         <input type="text"
-                            class="rounded-lg py-2 px-3 border 
+                            class="rounded-lg py-2 px-3 border
                           border-[#808080] border-opacity-50"
                             placeholder="Nama Gedung" />
                         <input type="text"
@@ -366,7 +315,7 @@
                             border-opacity-50"
                             placeholder="Jumlah Lantai " />
                         <select
-                            class="rounded-xl sm:w-auto text-[#808080] py-2 px-3 appearance-none 
+                            class="rounded-xl sm:w-auto text-[#808080] py-2 px-3 appearance-none
                         bg-transparent border border-[#808080] border-opacity-50">
                             <option value="" disabled selected>Status</option>
                             <option value="akademik">Aktif</option>
@@ -376,7 +325,7 @@
                 </div>
             </div>
             <div class="flex flex-col gap-5">
-                <textarea class="rounded-lg py-2 px-3 h-24 border border-[#808080] 
+                <textarea class="rounded-lg py-2 px-3 h-24 border border-[#808080]
                 border-opacity-50"
                     placeholder="Deskripsi"></textarea>
                 <button class="w-auto text-white bg-[red] py-2 rounded-xl font-extrabold">
