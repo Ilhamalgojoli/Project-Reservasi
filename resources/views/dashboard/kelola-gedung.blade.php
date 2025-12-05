2c91852ab8b75e3edbe93fdacb75de908db1d886
ilhom <rilhamgojali123@gmail.com>
2025-12-05 09:57:16 +0700

    update kelola gedung

--- resources/views/dashboard/kelola-gedung.blade.php
+++ resources/views/dashboard/kelola-gedung.blade.php
@@ -59,12 +59,14 @@
                     </div>
                 </div>
                 <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-2xl
+                        flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Ruangan</p>
                     </div>
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
+                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-2xl
+                        flex flex-col justify-center items-center">
                         <h1 class="text-white text-2xl">30</h1>
                         <p class="text-white font-bold text-sm">Jumlah</p>
                         <p class="text-white font-bold text-sm">Lantai</p>
@@ -72,216 +74,13 @@
                 </div>
             </div>
         </div>
-        <!-- Card One -->
-        <div class="bg-[#ffffff] text-black rounded-xl py-1 px-1
-                shadow-lg space-x-4 flex flex-col">
-            <div class="flex flex-col justify-center items-center gap-5 mb-2">
-                <div class="flex flex-col gap-2">
-                    <div class="relative w-auto h-64 card-container">
 
-                        <img src="{{ asset('assets/basila_images/gambar-gedung.png') }}"
-                            class="w-full h-full object-cover rounded-lg" />
-                        <div class="overlay absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-lg z-20 hidden"
-                            ></div>
-
-                        <div class="button-kelola absolute top-[110px] left-[150px] z-30 hidden">
-                            <a id="btn-gedung" href="{{ route("index7") }}"
-                                class="bg-[#FF0101] rounded-lg px-5 py-2 font-extrabold transition-all cursor-pointer
-                                duration-300 hover:scale-105 flex justify-center items-center gap-2 mx-auto text-white">
-                                <iconify-icon icon="mingcute:plus-fill" class="text-2xl"></iconify-icon>
-                                <span class="font-extrabold mt-1">Tambah Gedung</span>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="flex flex-col px-5 gap-3">
-                        <div class="flex flex-row gap-10 justify-between items-center">
-                            <h2 class="text-black font-bold leading-tight md:text-xl sm:text-md lg:text-2xl">Nama
-                                Gedung
-                            </h2>
-                            <iconify-icon icon="basil:edit-outline" class="text-4xl opacity-50"
-                                id="edit-gedung"></iconify-icon>
-                        </div>
-                        <p class=
-                               "text-[#8B8B8B] text-md">
-                            надвигается туман, надвигается туман, помоги мне, позволь ему помочь тебе, кожа
-                            на моем теле ослабевает, я хочу ее снять, я чувствую, как растут новые конечности, это
-                            больно,
-                            ...</p>
-                    </div>
-                </div>
-                <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Ruangan</p>
-                    </div>
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Lantai</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Card One -->
-        <div class="bg-[#ffffff] text-black rounded-xl py-1 px-1
-                shadow-lg space-x-4 flex flex-col">
-            <div class="flex flex-col justify-center items-center gap-5 mb-2">
-                <div class="flex flex-col gap-2">
-                    <div class="relative w-auto h-64 card-container">
-
-                        <img src="{{ asset('assets/basila_images/gambar-gedung.png') }}"
-                            class="w-full h-full object-cover rounded-lg" />
-                        <div class="overlay absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-lg z-20 hidden"
-                            ></div>
-
-                        <div class="button-kelola absolute top-[110px] left-[150px] z-30 hidden">
-                            <a id="btn-gedung" href="{{ route("index7") }}"
-                                class="bg-[#FF0101] rounded-lg px-5 py-2 font-extrabold transition-all cursor-pointer
-                                duration-300 hover:scale-105 flex justify-center items-center gap-2 mx-auto text-white">
-                                <iconify-icon icon="mingcute:plus-fill" class="text-2xl"></iconify-icon>
-                                <span class="font-extrabold mt-1">Tambah Gedung</span>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="flex flex-col px-5 gap-3">
-                        <div class="flex flex-row gap-10 justify-between items-center">
-                            <h2 class="text-black font-bold leading-tight md:text-xl sm:text-md lg:text-2xl">Nama
-                                Gedung
-                            </h2>
-                            <iconify-icon icon="basil:edit-outline" class="text-4xl opacity-50"
-                                id="edit-gedung"></iconify-icon>
-                        </div>
-                        <p class=
-                               "text-[#8B8B8B] text-md">
-                            надвигается туман, надвигается туман, помоги мне, позволь ему помочь тебе, кожа
-                            на моем теле ослабевает, я хочу ее снять, я чувствую, как растут новые конечности, это
-                            больно,
-                            ...</p>
-                    </div>
-                </div>
-                <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Ruangan</p>
-                    </div>
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Lantai</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Card One -->
-        <div class="bg-[#ffffff] text-black rounded-xl py-1 px-1
-                shadow-lg space-x-4 flex flex-col">
-            <div class="flex flex-col justify-center items-center gap-5 mb-2">
-                <div class="flex flex-col gap-2">
-                    <div class="relative w-auto h-64 card-container">
-
-                        <img src="{{ asset('assets/basila_images/gambar-gedung.png') }}"
-                            class="w-full h-full object-cover rounded-lg" />
-                        <div class="overlay absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-lg z-20 hidden"
-                            ></div>
-
-                        <div class="button-kelola absolute top-[110px] left-[150px] z-30 hidden">
-                            <a id="btn-gedung" href="{{ route("index7") }}"
-                                class="bg-[#FF0101] rounded-lg px-5 py-2 font-extrabold transition-all cursor-pointer
-                                duration-300 hover:scale-105 flex justify-center items-center gap-2 mx-auto text-white">
-                                <iconify-icon icon="mingcute:plus-fill" class="text-2xl"></iconify-icon>
-                                <span class="font-extrabold mt-1">Tambah Gedung</span>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="flex flex-col px-5 gap-3">
-                        <div class="flex flex-row gap-10 justify-between items-center">
-                            <h2 class="text-black font-bold leading-tight md:text-xl sm:text-md lg:text-2xl">Nama
-                                Gedung
-                            </h2>
-                            <iconify-icon icon="basil:edit-outline" class="text-4xl opacity-50"
-                                id="edit-gedung"></iconify-icon>
-                        </div>
-                        <p class=
-                               "text-[#8B8B8B] text-md">
-                            надвигается туман, надвигается туман, помоги мне, позволь ему помочь тебе, кожа
-                            на моем теле ослабевает, я хочу ее снять, я чувствую, как растут новые конечности, это
-                            больно,
-                            ...</p>
-                    </div>
-                </div>
-                <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Ruangan</p>
-                    </div>
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Lantai</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Card One -->
-        <div class="bg-[#ffffff] text-black rounded-xl py-1 px-1
-                shadow-lg space-x-4 flex flex-col">
-            <div class="flex flex-col justify-center items-center gap-5 mb-2">
-                <div class="flex flex-col gap-2">
-                    <div class="relative w-auto h-64 card-container">
-
-                        <img src="{{ asset('assets/basila_images/gambar-gedung.png') }}"
-                            class="w-full h-full object-cover rounded-lg" />
-                        <div class="overlay absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-lg z-20 hidden"
-                            ></div>
-
-                        <div class="button-kelola absolute top-[110px] left-[150px] z-30 hidden">
-                            <a id="btn-gedung" href="{{ route("index7") }}"
-                                class="bg-[#FF0101] rounded-lg px-5 py-2 font-extrabold transition-all cursor-pointer
-                                duration-300 hover:scale-105 flex justify-center items-center gap-2 mx-auto text-white">
-                                <iconify-icon icon="mingcute:plus-fill" class="text-2xl"></iconify-icon>
-                                <span class="font-extrabold mt-1">Tambah Gedung</span>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="flex flex-col px-5 gap-3">
-                        <div class="flex flex-row gap-10 justify-between items-center">
-                            <h2 class="text-black font-bold leading-tight md:text-xl sm:text-md lg:text-2xl">Nama
-                                Gedung
-                            </h2>
-                            <iconify-icon icon="basil:edit-outline" class="text-4xl opacity-50"
-                                id="edit-gedung"></iconify-icon>
-                        </div>
-                        <p class=
-                               "text-[#8B8B8B] text-md">
-                            надвигается туман, надвигается туман, помоги мне, позволь ему помочь тебе, кожа
-                            на моем теле ослабевает, я хочу ее снять, я чувствую, как растут новые конечности, это
-                            больно,
-                            ...</p>
-                    </div>
-                </div>
-                <div class="flex flex-row gap-4">
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Ruangan</p>
-                    </div>
-                    <div class="bg-[#FF0101] w-[120px] h-[100px] rounded-md flex flex-col justify-center items-center">
-                        <h1 class="text-white text-2xl">30</h1>
-                        <p class="text-white font-bold text-sm">Jumlah</p>
-                        <p class="text-white font-bold text-sm">Lantai</p>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
     <!-- Pop Up -->
     <section id="pop-up-gedung" class="hidden fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center z-50">
         <div class="bg-white p-6 rounded-xl shadow-lg
-                overflow-y-auto relative flex-col w-[90%] sm:w-[80%] md:w-[600px] lg:w-[700px]">
+                overflow-y-auto relative flex-col w-[900px] sm:w-[80%] md:w-[600px] lg:w-[700px]">
             <div class="flex flex-row justify-between mb-10">
                 <h1 class="text-2xl font-bold">Tambah Gedung</h1>
                 <button id="cls-btn-gedung">
