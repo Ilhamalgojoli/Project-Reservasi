118b3dbd777d9d87bcfcc491b4c0fe29fab07326
ilhom <rilhamgojali123@gmail.com>
2025-12-09 15:39:29 +0700

    update switch button

--- public/assets/js/switch-buttton.js
+++ public/assets/js/switch-buttton.js
@@ -2,12 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const akademik = document.querySelector('.akademik');
     const nonAkademik = document.querySelector('.non-akademik');
 
+    const tableAkademik = document.querySelector('.tableAkademik');
+    const tableNonAkademik = document.querySelector('.tableNonAkademik');
+
     akademik.addEventListener('click', () => {
         akademik.classList.add('bg-[#E35258]', 'text-white');
         nonAkademik.classList.remove('bg-[#E35258]', 'text-white');
 
         nonAkademik.classList.add('text-black');
         akademik.classList.remove('text-black');
+
+        tableAkademik.classList.remove('hidden');
+        tableNonAkademik.classList.add('hidden');
 
         console.log('clicked');
     });
@@ -18,6 +24,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         akademik.classList.add('text-black');
         nonAkademik.classList.remove('text-black');
+
+
+        tableAkademik.classList.add('hidden');
+        tableNonAkademik.classList.remove('hidden');
 
         console.log('clicked');
     });
