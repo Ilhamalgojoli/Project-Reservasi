215570248cbb4438832c5aa22570a89be07920d4
ilhom <rilhamgojali123@gmail.com>
2025-12-09 15:38:50 +0700

    Update js

--- public/assets/js/option-reservasi.js
+++ public/assets/js/option-reservasi.js
@@ -2,14 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const getOption = document.querySelector("#opsi-peminjaman");
     const idAkademik = document.getElementById("Akademik");
     const idNonAkademik = document.getElementById("Non-Akademik");
+    const judulForm = document.getElementById("value");
 
     const selectOption = (value) => {
         if (value === "Non-Akademik") {
             idNonAkademik.classList.remove("hidden");
             idAkademik.classList.add("hidden");
+            judulForm.innerHTML = "Non-Akademik";
+
+            console.log('Aktif');
         } else {
             idNonAkademik.classList.add("hidden");
             idAkademik.classList.remove("hidden");
+            judulForm.innerHTML = "Akademik";
+
+            console.log('Aktif');
         }
     };
 
