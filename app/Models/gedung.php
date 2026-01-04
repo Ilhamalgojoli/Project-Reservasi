c0009bdd69ea112d58404aeab0e39e3bc318b2e0
ilham <rilhamgojali123@gmail.com>
2026-01-04 18:39:55 +0800

    make model

--- app/Models/gedung.php
+++ app/Models/gedung.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class gedung extends Model
+class Gedung extends Model
 {
-    //
+    use HasFactory ;
+    
+    protected $table = 'gedungs';
+
+    protected $fillable = [
+        'nama_gedung',
+        'kode_gedung',
+        'jumlah_lantai',
+        'status',
+        'keterangan'
+    ];
 }
