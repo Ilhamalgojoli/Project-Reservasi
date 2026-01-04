404a698b388af966df8eb3308426f4d4903f4c62
ilham <rilhamgojali123@gmail.com>
2026-01-04 18:39:36 +0800

    Make migration

---
+++ database/migrations/2026_01_03_163943_create_gedungs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gedungs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_gedung');
+            $table->integer('jumlah_lantai');
+            $table->string('kode_gedung');
+            $table->enum('status', ["Aktif", "Tidak Aktif"])->default('Aktif');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gedungs');
+    }
+};
