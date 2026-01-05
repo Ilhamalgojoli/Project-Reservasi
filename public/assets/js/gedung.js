e16ce84009f2b9cb111b670cc809d2d0abf827b8
ilhom <rilhamgojali123@gmail.com>
2026-01-05 15:36:31 +0700

    update sweetalert

--- public/assets/js/gedung.js
+++ public/assets/js/gedung.js
@@ -1,3 +1,5 @@
+const { Result } = require("postcss");
+
 document.addEventListener('DOMContentLoaded', () => {
     const nama = document.getElementById('nama');
     const kode = document.getElementById('kode');
@@ -7,43 +9,68 @@ document.addEventListener('DOMContentLoaded', () => {
     const gambar = document.getElementById('gambar');
     const btnTambahGedung = document.getElementById('btn-submit');
 
-    btnTambahGedung.addEventListener('click', async(e) => {
-        try {
-            e.preventDefault();
+    btnTambahGedung.addEventListener('click', (e) => {
+        e.preventDefault();
 
-            const jumlahParsing = parseInt(jumlah.value);
-            console.log(status.value);
-
-            const formData = new FormData();
-
-            formData.append('id_gedung', kode.value);
-            formData.append('nama', nama.value);
-            formData.append('jumlah', jumlahParsing);
-            formData.append('status', status.value);
-            formData.append('keterangan', deskripsi.value);
-            formData.append('gambar', gambar.files[0]);
-
-            const req = await fetch(routes.storeData ,{
-                method: "POST",
-                headers: {
-                    "Accept": "application/json",
-                    "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').content 
-                },
-                body: formData
-            });
-
-            if(req.status === 403){
-                const errData = await req.json();
-                console.log(errData.message);
-            }
-
-            const data = req.json();
-
-            if(req.status === 200){
-                console.log(data.message);
-            }
-        } catch (err) {
-            console.error(err);
+        if (nama.value === "" || kode.value === "" ||
+            jumlah.value === "" || status.value === "" ||
+            deskripsi.value === "") {
+            alert("Silahkan isi form tambah gedung!");
+            return;
         }
+
+        Swal.fire({
+            title: 'Apakah Anda yakin?',
+            text: "Apakah Anda yakin ingin menyimpan data ini?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Konfirmasi',
+            cancelButtonText: 'Batal',
+            buttonsStyling: false,
+            reverseButtons: true,
+            customClass: {
+                confirmButton: 'btn-tetapkan',
+                cancelButton: 'btn-batal'
+            }
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    const jumlahParsing = parseInt(jumlah.value);
+                    console.log(status.value);
+
+                    const formData = new FormData();
+
+                    formData.append('id_gedung', kode.value);
+                    formData.append('nama', nama.value);
+                    formData.append('jumlah', jumlahParsing);
+                    formData.append('status', status.value);
+                    formData.append('keterangan', deskripsi.value);
+
+                    if (gambar.files.lenght > 0) {
+                        formData.append('gambar', gambar.files[0]);
+                    }
+
+                    const req = await fetch(routes.storeData, {
+                        method: "POST",
+                        headers: {
+                            "Accept": "application/json",
+                            "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').content
+                        },
+                        body: formData
+                    });
+
+                    const data = req.json();
+
+                    if (req.status === 200) {
+                        console.log(data.message);
+                    }
+                } catch (err) {
+                    if (req.status === 403) {
+                        const errData = await req.json();
+                        console.log(errData.message);
+                    }
+                }
+            }
+        });
     });
 });
