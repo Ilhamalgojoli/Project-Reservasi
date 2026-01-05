0eb8ebdd9dcb595845df480e827de7fd27a753db
ilhom <rilhamgojali123@gmail.com>
2026-01-05 10:51:43 +0700

    update minor menambahkan property pada class model

---
+++ app/Models/Gedung.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gedung extends Model
+{
+    use HasFactory ;
+    
+    protected $table = 'gedungs';
+
+    protected $fillable = [
+        'nama_gedung',
+        'kode_gedung',
+        'jumlah_lantai',
+        'status',
+        'keterangan',
+        'gambar'
+    ];
+}
