3e0802d6c937f0dd535edd72a448d8939d93bc0d
ilhom <rilhamgojali123@gmail.com>
2026-01-12 16:07:38 +0700

    model ruangan dan relasi dengan table asset serta join dengan table gedung

--- app/Models/Ruangan.php
+++ app/Models/Ruangan.php
@@ -5,21 +5,37 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Gedung;
+use App\Models\Lantai;
+use App\Models\Asset;
 
 class Ruangan extends Model
 {
     use HasFactory;
 
     protected $table = 'ruangans';
-
     protected $fillable = [
         'kode_ruangan',
         'status',
         'muatan_kapasitas',
-        'gedung_id'
+        'lantai_id'
     ];
 
+    public function lantai(){
+        return $this->belongsTo(Lantai::class);
+    }
+
+    public function asset(){
+        return $this->hasMany(Asset::class);
+    }
+
     public function gedung(){
-        return $this->belongTo(Gedung::class);
+        return $this->hasOneThrough(
+            Gedung::class,
+            Lantai::class,
+            'gedung_id',
+            'lantai_id',
+            'id',
+            'id'
+        );
     }
 }
