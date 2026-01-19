7babe2ba0d0d292cfe53f9d19dd40e98eb95c905
ilhom <rilhamgojali123@gmail.com>
2026-01-19 15:18:20 +0700

    membuat model peminjaman dan waktu peminjaman

---
+++ app/Models/DataPeminjaman.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\WaktuPeminjaman;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class DataPeminjaman extends Model
+{
+    use HasFactory;
+
+    protected $table = 'data_peminjaman';
+    protected $fillable =[
+        'fakultas',
+        'prodi',
+        'jenis_peminjaman',
+        'kode_matkul',
+        'lantai',
+        'ruangan',
+        'tanggal_peminjaman',
+        'jadwal_peminjaman',
+        'muatan',
+        'penanggung_jawab',
+        'kontak_penanggung_jawab',
+        'keterangan_peminjaman'
+    ];
+
+    public function waktuPeminjaman(){
+        return $this->hasMany(WaktuPeminjaman::class);
+    }
+}
---
+++ app/Models/WaktuPeminjaman.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\DataPeminjaman;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class WaktuPeminjaman extends Model
+{
+    use HasFactory;
+
+    protected $table = 'waktu_peminjaman';
+    protected $fillable = [
+        'waktu_peminjaman',
+        'jenis_peminjaman',
+        'peminjaman_id',
+    ];
+
+    public function peminjaman(){
+        return $this->belongsTo(DataPeminjaman::class);
+    }
+}
