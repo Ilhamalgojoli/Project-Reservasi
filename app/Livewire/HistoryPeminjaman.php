a53971d71e2a6157c19fe0c16f809a4a036dc1a2
ilhom <rilhamgojali123@gmail.com>
2026-02-04 09:36:50 +0700

    pindah render by livewire

--- app/Livewire/HistoryPeminjaman.php
+++ app/Livewire/HistoryPeminjaman.php
@@ -2,17 +2,35 @@
 
 namespace App\Livewire;
 
-use App\Http\Controllers\GetDataPeminjaman;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\DataPeminjaman;
 
 class HistoryPeminjaman extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        $get_data = new GetDataPeminjaman();
+        $get_data = new ApproveRejectBooking;
 
         return view('livewire.history-peminjaman', [
-            'peminjaman' => $get_data->getData('historyPeminjaman')
+            'peminjaman' => $get_data->getData('historyPeminjaman'),
         ]);
+    }
+
+    #[On('cancelBooking')]
+    public function cancelBooking($id)
+    {
+        $data_peminjaman = DataPeminjaman::findOrFail($id);
+
+        if ($data_peminjaman->status === 'Waiting') {
+            $data_peminjaman->delete();
+
+            $this->dispatch('successCancel', [
+                'text' => 'berhasil cancel booking'
+            ]);
+        }
     }
 }
