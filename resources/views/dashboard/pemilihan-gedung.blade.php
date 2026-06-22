059020ad6e88f2e00bbd59084a272145755f4a88
ilhom <rilhamgojali123@gmail.com>
2026-06-22 18:01:55 +0700

    Hapus fungsi script yang tidak terpakai

--- resources/views/dashboard/pemilihan-gedung.blade.php
+++ resources/views/dashboard/pemilihan-gedung.blade.php
@@ -3,10 +3,6 @@
     $title = 'Reservasi Ruangan';
 @endphp
 
-@push('extra_scripts')
-    <script src="{{ asset('assets/js/hover-card.js') }}" defer></script>
-@endpush
-
 @section('content')
     @livewire('user.pilih-gedung', [], key('pilih-gedung-' . url()->current()))
 @endsection
